cf6e7a5c9f807451a8613a5910d3229c3ad92e92
tourresh <tourresh@0fbda5ad-204a-4397-9e42-c173e0e86cc9>
2010-09-27 00:46:39 +0000

    Fixed the guns.lua that was screwing citygen. Simplecity also got a plot bug fixed. More plots added.

---
+++ trunk/data/plots/3x2/small_house_01.lua
@@ -0,0 +1,35 @@
+--Small_house_01
+--Author:Mendonca
+defineTile('p', 'PAVEMENT')
+defineTile('#', 'WALL_BRICK_DARK')
+defineTile('=', 'COUNTER_WOODEN_RED')
+defineTile(',', 'FLOOR_WOOD_TWO')
+defineTile('+', 'KITCHEN_SINK_RED')
+defineTile('D', 'DOOR_BRICK_DARK')
+defineTile('O', 'WINDOW_BRICK_DARK')
+defineTile('W', 'TOILET')
+defineTile('~', 'DARK_GRASS_ONE')
+defineTile('N', 'BASIN_WOODEN_RED')
+defineTile('H', 'SHOWER')
+defineTile('C', 'CHAIR_WOODEN')
+defineTile('T', 'TABLE_WOODEN')
+defineTile('B', 'BED_WOODEN_E')
+defineTile('b', 'BED_WOODEN_W')
+defineTile('', 'SHELF_METAL')
+defineTile('', 'SHELF_WOOD')
+defineTile('x', 'TELEVISION')
+defineTile('X', 'TELEVISION')
+defineTile('F', 'FRIDGE')
+defineTile('S', 'SOFA_DOWN_1')
+defineTile('s', 'SOFA_DOWN_2')
+
+return{
+[[pppppppppppppp]],
+[[p#########O##p]],
+[[p#WN#=+F#,Bb#p]],
+[[p#H,D,,,D,,,#p]],
+[[p####,C,#####p]],
+[[p#Ss,,T,#,Bb#p]],
+[[pO,,,,C,D,,,Op]],
+[[p#,X,,,,#,,x#p]],
+[[p###O#D###O##p]]}
---
+++ trunk/data/plots/4x4/medium_house_02.lua
@@ -0,0 +1,71 @@
+--MEDIUM HOUSE 2
+--Author Mendoca
+--4x4
+defineTile('p', 'PAVEMENT')
+defineTile('#', 'WALL_BRICK_DARK')
+defineTile('=', 'COUNTER_WOODEN_RED')
+defineTile(',', 'FLOOR_WOOD_TWO')
+defineTile('+', 'KITCHEN_SINK_RED')
+defineTile('D', 'DOOR_BRICK_DARK')
+defineTile('O', 'WINDOW_BRICK_DARK')
+defineTile('W', 'TOILET')
+defineTile('~', 'GRASS_DARK')
+defineTile('N', 'BASIN_WOODEN_RED')
+defineTile('H', 'SHOWER')
+defineTile('C', 'CHAIR_WOODEN')
+defineTile('T', 'TABLE_WOODEN')
+defineTile('B', 'BED_WOODEN_E')
+defineTile('b', 'BED_WOODEN_W')
+defineTile('E', 'BED_WOODEN_N')
+defineTile('e', 'BED_WOODEN_S')
+defineTile('/', 'SHELF_WOOD')
+defineTile('m', 'SHELF_METAL')
+defineTile('x', 'TELEVISION')
+defineTile('X', 'TELEVISION')
+defineTile('F', 'FRIDGE')
+defineTile('S', 'SOFA_UP_1')
+defineTile('s', 'SOFA_UP_2')
+defineTile('<', 'STAIR_WOODEN_UP')
+defineTile('>', 'STAIR_WOODEN_DOWN')
+defineTile('A', 'BATH_E')
+defineTile('a', 'BATH_W')
+defineTile('G', 'DOOR_GARAGE')
+defineTile('u', 'DESK_WOODEN')
+defineTile('-', 'FENCE_WOODEN')
+defineTile('t', 'LAMP_STANDARD')
+defineTile('v', 'PLANTPOT_FIXED')
+
+return{
+[[ppppppppppppppp]],
+[[p------~------p]],
+[[p-~#O#~~~#O#~-p]],
+[[p###,##D##,###p]],
+[[p#,,,,#,#,,X,#p]],
+[[pOCTC,D,D,,,,Op]],
+[[p#v,,,#,#Ss,t#p]],
+[[p######,######p]],
+[[p#+==F,,,,<#ppp]],
+[[pO,,,,,,#####p~]],
+[[p#t,,,,,D,,A#p~]],
+[[p##O##O##,,AOp~]],
+[[pppppppp#WNv#p~]],
+[[~~~~~~~p#####p~]],
+[[~~~~~~~ppppppp~]]}
+
+--Level One:
+
+--return{
+--[[]],
+--[[]],
+--[[]],
+--[[]],
+--[[   #O#   #O#]],
+--[[ ###,##O##,###]],
+--[[ #t,,,#,#,,,t#]],
+--[[ O,,,,D,D,,,,O]],
+--[[ #/,BB#,#BB,/#]],
+--[[ ######,######]],
+--[[ #t,,,,,,,>#]],
+--[[ O,,,,,,####]],
+--[[ #/,u,,v#]],
+--[[ ##O##O##]]}
