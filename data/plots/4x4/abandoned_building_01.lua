5c9eacadc84199a8142cb8721b8f7505b8576692
RodlenJack <RodlenJack@0fbda5ad-204a-4397-9e42-c173e0e86cc9>
2010-10-04 21:37:32 +0000

    added 2 of Nenjin's plots from the thread

---
+++ data/plots/4x4/abandoned_building_01.lua
@@ -0,0 +1,38 @@
+--Abandoned Building
+--Nenjin
+--20x20
+
+defineTile('P', 'FLOOR_PAVEMENT')
+defineTile('g', 'GRASS_DRY')
+defineTile('t', 'DIRT')
+defineTile('*', 'WALL_BRICK_DARK')
+defineTile('w', 'WINDOW_DARK_PLANK_BROKEN')
+defineTile('D', 'DOOR_METAL_RED')
+defineTile('r', 'RUBBLE_ONE')
+defineTile('R', 'RUBBLE_TWO')
+defineTile('.', 'CONCRETE_FLOOR')
+defineTile(',', 'CONCRETE_CRACK_1')
+defineTile(']', 'CONCRETE_CRACK_MOSS')
+
+return{
+PPPPPPPPPPPPPPPPPPPP
+PPPPPPPPPPPPPPPPPPPP
+PPP***w*******w**PPP
+PPPw.r,....,.]..*PPP
+PPP*...,.].,..,.wPPP
+PPP*R.....,R.,..*PPP
+PPP***D*******w**PPP
+PPP*.......r*ggggPPP
+PPP*r.,.],,.*gtttPPP
+PPPw....,...DgtttPPP
+PPP*...,.,..*gtttPPP
+PPP*.]......*ggggPPP
+PPP*****D*****w**PPP
+PPP*..R....r.,..*PPP
+PPP*..,.,].,....wPPP
+PPPw...R.,...R,.*PPP
+PPP***w*******w**PPP
+PPPPPPPPPPPPPPPPPPPP
+PPPPPPPPPPPPPPPPPPPP
+PPPPPPPPPPPPPPPPPPPP
+}
---
+++ data/plots/4x4/motel_02.lua
@@ -0,0 +1,42 @@
+--Cheap Motel
+--Nenjin
+-20x20
+
+defineTile('P', 'FLOOR_PAVEMENT')
+defineTile('*', 'WALL_BRICK')
+defineTile('D', 'DOOR_DARK_PLANK')
+defineTile(':', 'CONCRETE_CRACK_1')
+defineTile('.', 'CONCRETE_FLOOR')
+defineTile(',', 'CARPET_GRAY')
+defineTile(']', 'FLOOR_BIGCHECK_BLACK_DIRTY')
+defineTile('b', 'BED_WOODEN_W')
+defineTile('B', 'BED_WOODEN_E')
+defineTile('S', 'SOFA_DOWN_1')
+defineTile('s', 'SOFA_DOWN_2')
+defineTile('w', 'WINDOW_DARK_PLANK_BROKEN')
+defineTile('d', 'DESK_WOODEN')
+defineTile('c', 'CHAIR_WOODEN')
+defineTile('t', 'WC')
+
+return{
+PPPPPPPPPPPPPPPPPPPP
+PPPPPPPPPPPPPPPPPPPP
+PP****w**..***w***PP
+PP*]D,,,w..w,,,D]*PP
+PP*t*Bb,D.:D,Bb*t*PP
+PP*******::*******PP
+PP*]D,,,D.:D,Bb*t*PP
+PP*t*Bb,w..w,,,D]*PP
+PP*******..*******PP
+PP*]D,,,D:.D,Bb*t*PP
+PP*t*Bb,*..w,,,D]*PP
+PP*******..**w****PP
+PP*]D,,,w.::..:..*PP
+PP*t*Bb,D:.:...:.*PP
+PP*******..*D*w***PP
+PP*]D,,,D.:*,,,,,wPP
+PP*t*Bb,w:.wSs,dc*PP
+PP***w***..***w***PP
+PPPPPPPPPPPPPPPPPPPP
+PPPPPPPPPPPPPPPPPPPP
+}
