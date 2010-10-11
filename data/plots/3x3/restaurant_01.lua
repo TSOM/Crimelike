da60f3873c50eeb5526eba5a01413077c4ecf881
RodlenJack <RodlenJack@0fbda5ad-204a-4397-9e42-c173e0e86cc9>
2010-10-11 04:38:33 +0000

    more fixing!

--- data/plots/3x3/restaurant_01.lua
+++ data/plots/3x3/restaurant_01.lua
@@ -5,40 +5,42 @@
 defineTile('p', 'PAVEMENT')
 defineTile('#', 'WALL_DRY')
 defineTile('8', 'WALL_BRICK_DARK')
-defineTile('=', 'COUNTER_WOOD_RED')
+defineTile('=', 'FLOOR_WOOD_TWO', 'COUNTER_WOOD_RED')
+defineTile('_', 'FLOOR_CHECKERBOARD', 'COUNTER_WOOD_RED')
 defineTile(':', 'FLOOR_WOOD_TWO')
 defineTile('C', 'CONCRETE_FLOOR')
 defineTile(';', 'FLOOR_CHECKERBOARD')
 defineTile(',', 'FLOOR_BIGCHECK_GREEN')
 defineTile('+', 'SINK')
 defineTile('D', 'WALL_BRICK_DARK_DOOR')
-defineTile('d', 'DOOR_WOOD_DOUBLE')
+defineTile('d', 'WALL_DRY', 'DOOR_WOOD_DOUBLE')
 defineTile('O', 'WINDOW_BRICK_DARK')
-defineTile('W', 'WC')
+defineTile('W', 'FLOOR_BIGCHECK_GREEN', 'WC')
 defineTile('~', 'GRASS_ONE')
-defineTile('N', 'WHB')
+defineTile('N', 'FLOOR_BIGCHECK_GREEN', 'WHB')
 defineTile('H', 'SHOWER')
-defineTile('c', 'CHAIR_WOODEN')
-defineTile('T', 'TABLE_WOODEN')
+defineTile('c', 'FLOOR_WOOD_TWO', 'CHAIR_WOODEN')
+defineTile('T', 'FLOOR_WOOD_TWO', 'TABLE_WOODEN')
+defineTile('y', 'FLOOR_CHECKERBOARD', 'TABLE_WOODEN')
 defineTile('/', 'SHELF_WOOD')
 defineTile('%', 'SHELF_METAL')
 defineTile('x', 'TELEVISION')
 defineTile('X', 'TELEVISION')
-defineTile('F', 'FRIDGE')
-defineTile('e', 'OVEN')
+defineTile('F', 'FLOOR_CHECKERBOARD', 'FRIDGE')
+defineTile('e', 'FLOOR_CHECKERBOARD', 'OVEN')
 defineTile('-', 'FENCE_IRON')
 defineTile('t', 'LAMP_STANDARD')
 defineTile('v', 'PLANTPOT_FIXED')
 defineTile('M', 'HIFI')
-defineTile('?', 'CASH_REGISTER')
+defineTile('?', 'FLOOR_WOOD_TWO', 'COUNTER_WOOD_RED', 'CASH_REGISTER')
 
 return{
 [[ppppppppppppppp]],
 [[p8888888888888p]],
-[[p8=e=e=;;;dCCDp]],
+[[p8_e_e_;;;dCCDp]],
 [[pO;;;;;;;;#%%8p]],
 [[p8F;;;F#d####8p]],
-[[pOT;;;T#:d,,,Op]],
+[[pOy;;;y#:d,,,Op]],
 [[p8;;;;;#:#d#d8p]],
 [[p8#==###:#N#N8p]],
 [[p8v::cTc:#W#W8p]],
