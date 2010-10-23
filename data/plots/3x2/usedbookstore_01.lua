64effc334b54160034980dd7949c99172fb66bac
RodlenJack <RodlenJack@0fbda5ad-204a-4397-9e42-c173e0e86cc9>
2010-10-23 22:00:44 +0000

    Used book store!

---
+++ data/plots/3x2/usedbookstore_01.lua
@@ -0,0 +1,27 @@
+--Used Book Store
+--AUTHOR: Jack A T
+--15 x 10 plot
+
+defineTile('p', 'PAVEMENT')
+defineTile('#', 'WALL_BRICK_DARK')
+defineTile('=', 'COUNTER_WOOD_RED')
+defineTile('.', 'FLOOR_WOOD_THREE')
+defineTile('D', 'WALL_BRICK_DARK_DOOR')
+defineTile('O', 'WINDOW_BRICK_DARK')
+defineTile('g', 'GRASS_DARK')
+defineTile('S', 'SAFE_SIMPLE')
+defineTile('r', 'CASH_REGISTER')
+defineTile('s', 'SHELF_WOOD')
+defineTile('t', 'TREE_SMALL')
+
+return{
+[[ppppppppppppppp]],
+[[ptg#######O##gp]],
+[[pgg#...s.D..#gp]],
+[[ptg#.s.s.#S.#gp]],
+[[pgg#.s.s.####gp]],
+[[ptg#.s.s..=.#gp]],
+[[pggO.s.s..r.Ogp]],
+[[ptg#ss......#gp]],
+[[pgg#O#D###O##gp]],
+[[ppppppppppppppp]]}
