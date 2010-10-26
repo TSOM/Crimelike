97d332d4422c29cca9bd5371bcbb6b792cb1e1c7
extradave <extradave@0fbda5ad-204a-4397-9e42-c173e0e86cc9>
2010-10-26 18:48:35 +0000

    A swanky restaurant with an outside space, and a nice little house added.

---
+++ data/plots/4x4/small_house_nice_01.lua
@@ -0,0 +1,58 @@
+--Nice small house, 1
+--AUTHOR: MENDONCA
+--20 x 20 plot
+
+defineTile('p', 'PAVEMENT')
+defineTile('8', 'WALL_BRICK')
+defineTile('#', 'WALL_DRY')
+defineTile('=', 'COUNTER_WOOD_RED')
+defineTile('+', 'SINK')
+defineTile('F', 'FRIDGE')
+defineTile('Z', 'TABLE_WOODEN', 'FLOOR_WOOD_THREE')
+defineTile('c', 'CHAIR_WOODEN', 'FLOOR_WOOD_THREE')
+defineTile(',', 'GRASS_LAWN')
+defineTile('.', 'FLOOR_BRICK_CLAY_ONE')
+defineTile(';', 'FLOOR_BRICK_GENERIC')
+defineTile(':', 'FLOOR_WOOD_THREE')
+defineTile('D', 'DOOR_FANCY_2', 'WALL_BRICK')
+defineTile('d', 'DOOR_APART_1', 'WALL_DRY')
+defineTile('O', 'WINDOW_HOUSE', 'WALL_BRICK')
+defineTile('W', 'WC', 'FLOOR_BRICK_GENERIC')
+defineTile('~', 'GRASS_ONE')
+defineTile('-', 'FENCE_WOODEN', 'GRASS_LAWN')
+defineTile('N', 'WHB', 'FLOOR_BRICK_GENERIC')
+defineTile('H', 'SHOWER', 'FLOOR_BRICK_GENERIC')
+defineTile('T', 'TREE_SMALL', 'GRASS_LAWN')
+defineTile('Y', 'TREE_SMALL', 'GRASS_ONE')
+defineTile('%', 'SHELF_WOOD')
+defineTile('&', 'SHELF_METAL')
+defineTile('S', 'SOFA_UP_1', 'FLOOR_WOOD_THREE')
+defineTile('s', 'SOFA_UP_2', 'FLOOR_WOOD_THREE')
+defineTile('o', 'WASHING_MACHINE')
+defineTile('v', 'PLANTPOT_FIXED')
+defineTile('f', 'STANDARD_LAMP', 'FLOOR_WOOD_THREE')
+defineTile('t', 'STANDARD_LAMP', 'FLOOR_BRICK_GENERIC')
+defineTile('B', 'BED_WOODEN_E', 'FLOOR_BRICK_GENERIC')
+defineTile('b', 'BED_WOODEN_W', 'FLOOR_BRICK_GENERIC')
+
+return{
+[[pppppppppppppppppppp]],
+[[p~~~~~~~~~~Y~~~~~~~p]],
+[[p~~~~~~~~~~~~~~~~~~p]],
+[[p~~88OD8O8O88O88~~~p]],
+[[p~~8:::=+F=#tBb8~~~p]],
+[[p~~O:::::::#;;;O~Y~p]],
+[[p~~8:::::c:d;;%8~~~p]],
+[[p~~8::Ss:Z:####8~~~p]],
+[[p~~O:::::::d;;HO~~~p]],
+[[p~~8f::x::v#;WN8~~~p]],
+[[p~~88O8##d##8888~~~p]],
+[[p~~-,,8&&...D,,-~~Yp]],
+[[p~~-,,O....o8,,-~~~p]],
+[[p~~-,,88O8O88,,-~~~p]],
+[[p~~-,,,,,,,,,,,-~Y~p]],
+[[p~~-,,,,,,,,,,,-~~~p]],
+[[p~~-,,T,,,,,,T,-~~~p]],
+[[p~~-,,,,,,,,,,,-~~~p]],
+[[p~~-------------~~~p]],
+[[pppppppppppppppppppp]]}
