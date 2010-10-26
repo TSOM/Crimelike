e5ed04b7a79f55d305aa76a6ff7e090524a44f54
extradave <extradave@0fbda5ad-204a-4397-9e42-c173e0e86cc9>
2010-10-26 18:48:35 +0000

    A swanky restaurant with an outside space, and a nice little house added.

---
+++ trunk/data/plots/5x4/restaurant_02.lua
@@ -0,0 +1,60 @@
+--Restaurant with Courtyard, 2
+--AUTHOR: MENDONCA
+--25 x 20 plot
+
+defineTile('p', 'PAVEMENT')
+defineTile('8', 'WALL_SLATTED')
+defineTile('9', 'WALL_DRY')
+defineTile('_', 'COUNTER_WOOD_RED')
+defineTile('=', 'COUNTER_METAL')
+defineTile('?', 'CASH_REGISTER', 'COUNTER_WOOD_RED')
+defineTile('+', 'SINK_METAL')
+defineTile('F', 'FRIDGE')
+defineTile('Y', 'TABLE_WOODEN', 'FLOOR_WOOD_THREE')
+defineTile('T', 'TABLE_WOODEN', 'PAVEMENT')
+defineTile('z', 'CHAIR_WOODEN', 'FLOOR_WOOD_THREE')
+defineTile('h', 'CHAIR_WOODEN', 'FLOOR_BRICK_GENERIC')
+defineTile('c', 'CHAIR_WOODEN', 'PAVEMENT')
+defineTile('u', 'DESK_WOODEN')
+defineTile('.', 'FLOOR_BRICK_GENERIC')
+defineTile(',', 'FLOOR_WOOD_THREE')
+defineTile('D', 'DOOR_GLASS', 'WALL_SLATTED')
+defineTile('d', 'DOOR_APART_1', 'WALL_DRY')
+defineTile('O', 'WINDOW_HOUSE', 'WALL_SLATTED')
+defineTile('H', 'WINDOW_STORE', 'WALL_SLATTED')
+defineTile('W', 'WC', 'FLOOR_BRICK_GENERIC')
+defineTile('~', 'GRASS_ONE')
+defineTile('N', 'WHB', 'FLOOR_BRICK_GENERIC')
+defineTile('H', 'SHOWER', 'FLOOR_BRICK_GENERIC')
+defineTile('%', 'SHELF_WOOD')
+defineTile('&', 'SHELF_METAL')
+defineTile('S', 'SOFA_UP_1', 'FLOOR_WOOD_THREE')
+defineTile('s', 'SOFA_UP_2', 'FLOOR_WOOD_THREE')
+defineTile('o', 'SERVING_HATCH', 'DRY_WALL')
+defineTile('v', 'PLANTPOT_FIXED')
+defineTile('$', 'SAFE_SIMPLE', 'FLOOR_BRICK_GENERIC')
+defineTile('"', 'OVEN')
+defineTile('w', 'WATER')
+defineTile('-', 'LILYPAD', 'WATER')
+
+return{
+[[ppppppppppppppppppppppppp]],
+[[p~~~~~~~~~~~~~~~~~~~~~~~p]],
+[[p~~88O88O88O8O88O8O8O8O8p]],
+[[p~~8W.9,,,,,,,,,,9=F+F=8p]],
+[[p~~ON.9,,,,zYz,,z9.....Op]],
+[[p~~89d9__?,,,,,,Y99..=.8p]],
+[[p~~8,,,,,,,zYz,,z,9....8p]],
+[[p~~O,zYz,,,,,,,,,,9..=.Op]],
+[[p~~8v,,,,,,zYz,,,v9....8p]],
+[[p~~8HHHGGHHHH8,,,,o..=.8p]],
+[[p~~ppppppppppHz,z,o....Op]],
+[[p~~pcTcpcTcppHY,Y,9..=.8p]],
+[[p~~ppppppppppHz,z,9....8p]],
+[[pppppppppppppHv,,,9..."Op]],
+[[~~~~~~~~~p8O8899d99..."8p]],
+[[~wwwwwww~p8u%%....9...F8p]],
+[[~w-wwwww~pOh......d....Op]],
+[[~wwwww-w~p8..$....9....8p]],
+[[~wwwwwww~p8O8O88O8O8O8O8p]],
+[[~~~~~~~~~pppppppppppppppp]]}   
